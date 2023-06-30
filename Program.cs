22f85f9d99153b6d65c75d2e5d3c04d7e0b60244
Niapa <niapa7@yandex.ru>
2023-06-30 19:55:26 +0700

    1-2 commit

--- Program.cs
+++ Program.cs
@@ -8,42 +8,6 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
-int InputNum(string message)
-{
-    Console.Write(message);
-    return int.Parse(Console.ReadLine()!);
-}
-
-double[,] Create2DArray(int rows, int cols)
-{
-    return new double[rows, cols];
-}
-void Fill2DArray(double[,] array, int min, int max)
-{
-    Random rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = rnd.NextDouble(min, max + 1);
-}
-
-void Print2DArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i, j]}\t");
-        Console.WriteLine();
-    }
-}
-int rows = InputNum("Введите количество строк: ");
-int columns = InputNum("Введите количество столбцов: ");
-int minValue = InputNum("Введите минимальное значение диапазона: ");
-int maxValue = InputNum("Введите максимальное значение диапазона: ");
-
-double[,] myArray = Create2DArray(rows, columns);
-Fill2DArray(myArray, minValue, maxValue);
-Print2DArray(myArray);
-
 /*
 
 Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
@@ -61,3 +25,39 @@ Print2DArray(myArray);
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+double[,] Create2DArray(int rows, int cols)
+{
+    return new double[rows, cols];
+}
+void Fill2DArray(double[,] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.NextDouble();
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+double[,] myArray = Create2DArray(rows, columns);
+Fill2DArray(myArray);
+Print2DArray(myArray);
+
+
